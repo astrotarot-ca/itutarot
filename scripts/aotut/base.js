bd883f0711096d830658a5c05b95d13bb8022a3b
Seraphim Dos Santos <cyb3rsh4t@gmail.com>
2019-08-02 21:11:48 -0400

    forgot js file

--- scripts/aotut/base.js
+++ scripts/aotut/base.js
@@ -2,6 +2,11 @@
  *	author: Serafim Dos Santos Fagundes
  */
 $(function () {
+	var aImg = document.getElementsByClassName("aspect");
+	for (i=0; i<aImg.length; i++) {
+		dragElement(aImg[i]);
+	}
+
 	var aImg = document.getElementsByClassName("house");
 	for (i=0; i<aImg.length; i++) {
 		dragElement(aImg[i]);
